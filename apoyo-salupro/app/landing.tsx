41e3288c66c968776f84c469d6970ba730780593
ElAdagioDeJP <juan.vargasr432@gmail.com>
2026-06-25 18:12:15 -0400

    fix(landing): + de nextec como superíndice junto a la c

--- apoyo-salupro/app/landing.tsx
+++ apoyo-salupro/app/landing.tsx
@@ -562,11 +562,7 @@ function SiteFooter() {
             <span className="text-gray-500">Desarrollado por</span>
             <Image src="/nextec.svg" alt="nextec" width={20} height={20} className="h-5 w-5 transition-transform group-hover:rotate-90" />
             <span className="font-semibold text-gray-200 group-hover:text-white transition-colors tracking-wide lowercase">
-              nexte
-              <span className="relative inline-block">
-                <span aria-hidden className="absolute -top-1.5 -left-1 text-[0.62em] font-bold leading-none">+</span>
-                c
-              </span>
+              nexte<sup className="text-[0.6em] font-bold align-super -mr-px">+</sup>c
             </span>
           </a>
         </div>
